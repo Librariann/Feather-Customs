7ec60a54be1af18a9a646529266facf09d66b3ce
Librariann <okpc0306@naver.com>
2026-03-17 12:00:00 +0900

    scripts/bird_data.gd 추가

---
+++ scripts/bird_data.gd
@@ -0,0 +1,8 @@
+class_name BirdData
+extends Resource
+
+@export var bird_type: String
+@export var destination: String
+@export var passport: PassportData
+@export var stamp: StampData
+@export var baggage: Array[String]

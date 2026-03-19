80d09b1e3f2b31b89d73e20e8b2f7f873fedf54d
Librariann <okpc0306@naver.com>
2026-03-19 12:00:00 +0900

    scripts/passport_data.gd 추가

---
+++ scripts/passport_data.gd
@@ -0,0 +1,8 @@
+class_name PassportData
+extends Resource
+
+@export var bird_name: String
+@export var species: String
+@export var destination: String
+@export var baggage_tags: Array[String]
+@export var is_valid: bool = true

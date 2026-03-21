00cd5a90819eadb51aa81b7aa5757f736796ec27
Librariann <okpc0306@naver.com>
2026-03-21 12:00:00 +0900

    scripts/rule_data.gd 추가

---
+++ scripts/rule_data.gd
@@ -0,0 +1,8 @@
+class_name RuleData
+extends Resource
+
+@export var rule_name: String
+@export var description: String
+@export var effect_type: String
+@export var target: String
+@export var value: int
